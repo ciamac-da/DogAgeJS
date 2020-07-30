17e6d26cddf5787ca8cab8034ca1894442bc3b9a
the-last-martian <the-last-martian@martian.localdomain>
2020-07-30 15:23:39 +0200

    hmmm

--- dog.js
+++ dog.js
@@ -1,3 +1,18 @@
 function cyears(form) {
     form.dogage.value = form.myage.value *7
 }
+
+
+
+
+function random_bg_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+ console.log(bgColor);
+  
+    document.getElementById("body").style.background = bgColor;
+    }
+
+random_bg_color();
